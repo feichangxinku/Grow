dd24b98b365d1cab7edf995910822e9e3c1c4f13
灵枝 <zichun.zzc@antfin.com>
2018-04-17 21:52:24 +0800

    add test files and class

---
+++ src/main/java/com/iris/grow/Score2.java
@@ -0,0 +1,31 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2018 All Rights Reserved.
+ */
+package com.iris.grow;
+
+/**
+ *
+ * @author zichun
+ * @version $Id: Score2.java, v 0.1 2018年04月17日 下午9:44 zichun Exp $
+ */
+public class Score2 {
+    //Input size
+    private int dim_size;
+    private int q_size;
+
+    //Input parameters
+    private double[][] WOQ_mat;
+    private double[] SOQ_list;
+    private double[] WOD_mat;
+
+    //Output score
+    private double[] SOD_mat;
+    private int[] Trans_SOD_mat;
+
+    private double GS;
+    private int GS_int;
+
+    public int[] GetDimScores(double[][]WOQ_mat,double[]SOQ_list,double[]WOD_mat)
+
+}
---
+++ src/main/java/com/iris/grow/com/iris/grow/test/IMatrix.java
@@ -0,0 +1,13 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2018 All Rights Reserved.
+ */
+package com.iris.grow.com.iris.grow.test;
+
+/**
+ *
+ * @author zichun
+ * @version $Id: IMatrix.java, v 0.1 2018年04月17日 下午4:47 zichun Exp $
+ */
+public interface IMatrix {
+}
